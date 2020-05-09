ee98742992ac49d6e8fc4ccb95cbd1b4cd014e7b
anselbrandt <ansel@anselbrandt.com>
2020-05-08 22:22:20 -0400

    Pipeline multiple image filters

--- src/ImageFilter.js
+++ src/ImageFilter.js
@@ -17,9 +17,25 @@ export default function ImageFilter(props) {
 
     const filter = gpu.createKernelMap(
       function (image) {
-        const pixel = image[this.thread.y][this.thread.x];
+        let pixel = image[this.thread.y][this.thread.x];
         const gray = 0.2126 * pixel[0] + 0.7152 * pixel[1] + 0.0722 * pixel[2];
         this.color(gray, gray, gray, pixel[3]);
+      },
+      {
+        graphical: true,
+        output: [width, height],
+        pipeline: true,
+      }
+    );
+
+    const kernel = gpu.createKernel(
+      function (videoFrame) {
+        if (this.thread.y < 10 && this.thread.x < 10) {
+          this.color(1, 0, 0, 1);
+        } else {
+          const pixel = videoFrame[this.thread.y][this.thread.x];
+          this.color(pixel[0], pixel[1], pixel[2], pixel[3]);
+        }
       },
       {
         graphical: true,
@@ -30,9 +46,9 @@ export default function ImageFilter(props) {
     canvas.width = canvas.clientWidth;
     canvas.height = canvas.clientHeight;
     image.addEventListener("load", () => {
-      filter(image);
+      kernel(filter(image));
     });
-    return () => image.removeEventListener("load", () => filter(image));
+    return () => image.removeEventListener("load", () => kernel(filter(image)));
   }, [inputRef, outputRef, width, height]);
 
   return (
