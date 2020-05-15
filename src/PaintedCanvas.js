a0c3b8a8bc3309bab4d2e745013e871fa8452e8d
anselbrandt <ansel@anselbrandt.com>
2020-05-15 04:15:48 -0400

    Paint image to canvas from imageData

--- src/PaintedCanvas.js
+++ src/PaintedCanvas.js
@@ -1,30 +1,42 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
+import { GPU } from "gpu.js";
+
 import styles from "./App.module.css";
-import image_1 from "./image_1.png";
 
 export default function ImageFilter(props) {
   const { inputRef, outputRef, flowRef, width, height } = props;
 
   useEffect(() => {
-    const imageElement = inputRef.current;
-    const canvasElement = outputRef.current;
-    const pixelImage = flowRef.current;
+    const imageElement1 = inputRef.current;
+    const imageElement2 = outputRef.current;
+    const flowElement = flowRef.current;
 
-    canvasElement.width = canvasElement.clientWidth;
-    canvasElement.height = canvasElement.clientHeight;
+    imageElement1.width = imageElement1.clientWidth;
+    imageElement1.height = imageElement1.clientHeight;
 
-    pixelImage.width = pixelImage.clientWidth;
-    pixelImage.height = pixelImage.clientHeight;
+    imageElement2.width = imageElement2.clientWidth;
+    imageElement2.height = imageElement2.clientHeight;
 
-    const img = new Image();
-    img.src = "/image_1.png";
-    const context = canvasElement.getContext("2d");
-    const pixelContext = pixelImage.getContext("2d");
-    img.onload = function () {
-      context.drawImage(img, 0, 0, width, height);
-      const imgData = context.getImageData(0, 0, width, height).data;
-      const data = new ImageData(new Uint8ClampedArray(imgData), width, height);
-      pixelContext.putImageData(data, 0, 0);
+    flowElement.width = flowElement.clientWidth;
+    flowElement.height = flowElement.clientHeight;
+
+    const context1 = imageElement1.getContext("2d");
+    const context2 = imageElement2.getContext("2d");
+    const flowContext = flowElement.getContext("2d");
+
+    const img1 = new Image();
+    const img2 = new Image();
+    img1.src = "/image_1.png";
+    img2.src = "/image_2.png";
+
+    img1.onload = () => {
+      context1.drawImage(img1, 0, 0, width, height);
+      const img1Data = context1.getImageData(0, 0, width, height);
+      img2.onload = () => {
+        context2.drawImage(img2, 0, 0, width, height);
+        const img2Data = context2.getImageData(0, 0, width, height);
+        flowContext.putImageData(img1Data, 0, 0);
+      };
     };
   }, [inputRef, outputRef, flowRef, width, height]);
 
